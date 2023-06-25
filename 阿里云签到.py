2c1bcdfa5a37b4c636b7a5363275e89603cb0869
aiofy <1993445592@qq.com>
2023-06-25 15:19:18 +0800

    修复

--- 阿里云签到.py
+++ 阿里云签到.py
@@ -7,7 +7,7 @@ from loguru import logger
 from notify import send
 
 """
-自行添加环境变量 ALI_PAN={"refresh_token": "ff7fd57d0ff943b4b43c83b99ed78c6e"}
+自行添加环境变量 ALI_PAN={"refresh_token": "xxxxxxxxxxxxxxxxxxxxxxxx"}
 """
 
 TITLE = "阿里云盘签到"
